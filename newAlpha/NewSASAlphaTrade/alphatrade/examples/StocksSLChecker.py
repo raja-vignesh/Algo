07e354099832124ff46a91ffcb207bdd88e493a8
tamilCodingWizard <tamilcodingwizard@gmail.com>
2024-05-12 23:10:52 +0530

    changes

--- newAlpha/NewSASAlphaTrade/alphatrade/examples/StocksSLChecker.py
+++ newAlpha/NewSASAlphaTrade/alphatrade/examples/StocksSLChecker.py
@@ -6,8 +6,7 @@ from time import sleep
 import os
 from datetime import datetime,timedelta
 import pandas as pd
-import numpy as np
-import pandas_ta as ta 
+
 import numpy as np
 import json
 import collections
